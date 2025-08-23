62afb5fbd808bc0d74cad1f955529ce9cca54431
samuel <sml.sbjr@gmail.com>
2025-08-23 12:47:51 -0300

    update

--- src/omp-3.5.0-1/plugins/generic/webFeed/.github/actions/tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-npx cypress run  --headless --browser chrome  --config '{"specPattern":["plugins/generic/webFeed/cypress/tests/functional/*.cy.js"]}'
-
-
--- src/ops-3.4.0-9/plugins/generic/webFeed/.github/actions/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-
-npx cypress run --headless --browser chrome --config '{"specPattern":["plugins/generic/webFeed/cypress/tests/functional/*.cy.{js,jsx,ts,tsx}"]}'
-
-
